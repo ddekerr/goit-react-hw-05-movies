3c2cabb79720cb8e0ac6005d621dfd0d3e246219
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-11-17 16:00:13 +0200

    Prop Types

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,4 +1,5 @@
 import { MovieListContainer, MovieItem, MovieLink } from './MovieList.styled';
+import PropTypes from 'prop-types';
 
 export const MovieList = ({ movies, homepage, location }) => {
   return (
@@ -6,12 +7,25 @@ export const MovieList = ({ movies, homepage, location }) => {
       {movies.map(({ id, title }) => (
         <MovieItem key={id}>
           {homepage ? (
-            <MovieLink to={`movies/${id}`} state={{ from: location}}>{title}</MovieLink>
+            <MovieLink to={`movies/${id}`} state={{ from: location }}>
+              {title}
+            </MovieLink>
           ) : (
-            <MovieLink to={`${id}`} state={{ from: location}}>{title}</MovieLink>
+            <MovieLink to={`${id}`} state={{ from: location }}>
+              {title}
+            </MovieLink>
           )}
         </MovieItem>
       ))}
     </MovieListContainer>
   );
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ),
 };
